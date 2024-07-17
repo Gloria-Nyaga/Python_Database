d042a93e75b47290a8691ca5cd05dc760e31c374
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-07-17 17:34:02 +0300

    made chhanges

--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,7 @@ from student.models import Student
 
 class Courses(models.Model):
     course_title = models.CharField(max_length = 20)
-    course_category = models.TextField (max_length = 20)
+    course_category = models.CharField (max_length = 20)
     course_start_date = models.DateField ()
     course_end_date = models.DateField ()
     course_code = models.PositiveSmallIntegerField ()
